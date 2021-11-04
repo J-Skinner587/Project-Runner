918ad18f29e471a7647c493ab7fe38f5c3843143
J-Skinner64 <521554@chrome.escg.ac.uk>
2021-11-04 16:11:32 +0000

    Almost done animation. Added tiny bit to level.

--- Assets/playerControllwe.cs
+++ Assets/playerControllwe.cs
@@ -4,20 +4,65 @@ using UnityEngine;
 
 public class playerControllwe : MonoBehaviour
 {
-    public float speed;
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {Idle, Running, jumping}
+    private State state = State.Idle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        float hDirection = Input.GetAxis("Horizontal");
+        
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-10, 10);
+            anim.SetBool("Running", true);
+        }
 
-        rb.velocity = new Vector2(x * Time.deltaTime * speed, rb.velocity.y);
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(10, 10);
+            anim.SetBool("Running", true);
+        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
 
+        VelocityState();
+        anim.SetInteger("State", (int)state);
+    }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+
+        }
+
+        if(Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
+        {
+            //Going Right
+            state = State.Running;
+        }
+
+        else if(rb.velocity.x < .1f)
+        {
+            //going left
+        }
     }
 }
-
