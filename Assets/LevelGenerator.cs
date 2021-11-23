64f8469a0380d79bdae6d89553c2e75e8bf84b05
J-Skinner64 <521554@chrome.escg.ac.uk>
2021-11-23 15:56:48 +0000

    Switched Tutorial for random level.

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform levelPart_1;
+
+    private void awake()
+    {
+        Instantiate(levelPart_1, new Vector3(38, 4, -83), Quaternion.identity);
+    }
+
+}
--- Assets/PlatformGenerator.cs
+++ Assets/PlatformGenerator.cs
@@ -12,19 +12,19 @@ public class PlatformGenerator : MonoBehaviour
     private float platformWidth;
 
     // Start is called before the first frame update
-    void Start()
+    void Start  ()
     {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void Update  ()
     {
-        if (transform.position.x < generationPoint.position.x)
+        if(transform.position.x < generationPoint.position.x)
         {
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
-            Instantiate(thePlatform, transform.position, transform.rotation);
+            Instantiate  (thePlatform, transform.position, transform.rotation);
         }
     }
 }
