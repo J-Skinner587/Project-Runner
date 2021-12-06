f8dbe64e2d44b288712a4b72226b64f5ed853617
J-Skinner64 <521554@chrome.escg.ac.uk>
2021-12-06 11:56:58 +0000

    Did sound. Also started on level.

--- Assets/EnemyAU.cs
+++ Assets/EnemyAU.cs
@@ -53,8 +53,13 @@ public class EnemyAU : MonoBehaviour
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
         Vector2 force = direction * speed * Time.deltaTime;
 
+        rb.AddForce(force);
+
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
         if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
 }
